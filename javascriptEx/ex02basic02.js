e80e1abb2b3148e535a9dd158766efac4cada0de
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-01-11 11:50:11 +0900

    .
    .

--- javascriptEx/ex02basic02.js
+++ javascriptEx/ex02basic02.js
@@ -34,13 +34,18 @@ console.log(isDead);
 console.log(!isDead);
 console.log(!!isDead);
 
-// falsy value 
+// falsy value : 값으로 인식하지 않는 값 
 
 console.log(!!0); // 숫자 0 
 console.log(!!""); // 문자 빈문자열 
-console.log(!!null);
-console.log(!!undefined);
+console.log(!!null); // 비어있는 주소값 
+console.log(!!undefined); 
 console.log(!!NaN);
+
+// let cat = undefined; -> 쓰지 마세요 
+//cat; // 위에 있는 것을 축약하면 동일한 것 
+
+let cat = null;  // 비어있다는 표시 null로만 표시하기!! 
 
 
 // truly value 
@@ -48,6 +53,56 @@ console.log(!!NaN);
 console.log(!!10); // 0 제외한 모든 숫자 
 console.log(!!-10.123); // 음수도 동일
 console.log(!!"test"); // 비어있지 않는 모든 문자 
-console.log(!![]);
-console.log(!!{});
+console.log(!![]);  // new Array();   new ArrayList();
+console.log(!!{}); // new Object();
 console.log(!!Infinity);
+
+// symbol 타입 : 유일무이한 값 
+const num1 = '1';
+const num2 = '1';
+console.log(num1 == num2);
+const symbol1 = Symbol('1'); 
+const symbol2 = Symbol('1'); 
+console.log(symbol1);
+console.log(symbol2);
+console.log(symbol1 == symbol2);  // false 
+
+// 오브젝트 타입 -> java Map : key- value 이뤄져있다 
+
+// const dog ={  key 값에서는 원시값(: 주의!! String은 JS에서 원시값임 )만 들어갈 수 있는데 왠만하면 String 만 사용하자 
+//   'name' : '바둑이',
+//   10 : 4,
+//   true : true
+// }
+
+let dog = {
+  10 :'십',
+  undefined : '정의되지않는값 ',
+  null : '비어있는값',
+  // [] : '참조값은 key로 못쓴다 '
+}
+console.log(dog[10])   // hash.getValue(apple) // 1000;
+
+dog = { 'name': '바둑이', age: 4, isOwner: true };  // 그냥 쓰면 String 으로 자동으로 key값 인식한다 
+console.log(dog); // { name: '바둑이', age: 4, isOwner: true }
+
+// 오브젝트 값(속성값) 접근 방법 2가지 
+console.log(dog.name);
+console.log(dog["name"]);  // dog[name] 하면 에러 뜬다 꼭 스트링으로 key값 넣어주기 ! 
+
+function getKeyValue(object, key){
+  console.log(object[key]);
+}
+function getKeyValue2(object, key){
+  console.log(object.key); // 순수하게 key 하는 속성값을 객체 안에서 찾는것 : 동적으로 못 바꾼다 
+
+  if(key == 'age'){
+    console.log(object.age);
+  }
+}
+
+getKeyValue(dog , 'age');
+getKeyValue(dog , 'isOwner');
+getKeyValue(dog , 'name');
+
+getKeyValue2(dog,'age');
