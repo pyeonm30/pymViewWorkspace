1d6246e66521c31b6f3e4e27612718be177f007f
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-01-12 17:24:31 +0900

    .
    .

---
+++ javascriptEx/ex28.js
@@ -0,0 +1,14 @@
+var num1 = 10;
+var num2 = 20;
+
+function add(x, y) {
+  var result = x + y;
+  return result;
+}
+var mutlply = function(x, y) {
+  var result = x * y;
+  return result;
+}
+
+var res1 = add(num1, num2);
+var res2 = mutlply(num1, num2);
