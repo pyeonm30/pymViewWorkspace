8980c5b1c4550690567e1f586964a1fe1e73aaf3
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-01-12 11:15:21 +0900

    .
    .

--- javascriptEx/ex06loops.js
+++ javascriptEx/ex06loops.js
@@ -19,7 +19,7 @@ do {
 } while (i > 90);
 
 
-// for - in : key 출력하는 for문 : 배열에서 키는 indext 출력 , 객체 key 는 key가 나오는것 
+// for - in : key 출력하는 for문 : 배열에서 키는 index 출력 , 객체 key 는 key가 나오는것 
 
 let array = [10, 20, 30, 40, 50, 60]; // 왠만하면 어레이에 같은 타입만 쓰자!! 
 
@@ -29,7 +29,11 @@ for (let i in array) {
 console.log('----------------')
 
 // for - of : iterable 한 객체만 사용할 수 있다 : looping 할 수 있는 타입만 가능하다 
-// 객체는 사용 못하고 배열만 사용할 수 있다. 
+// 객체는 사용 못하고 배열만 사용할 수 있다.
+
+// 자바에서 향상된 for문과 같아 
+
+// for(int num : array) {} 
 for (let i of array) {
   console.log(i);
 }
--- javascriptEx/ex16testBasic.js
+++ javascriptEx/ex16testBasic.js
@@ -5,12 +5,42 @@ console.log(Math.random() * 100); // 0~ 99
 // 1 ~100 
 console.log(Math.random() * 10 + 1); //( 0~ 9) + 1 => 1 ~ 10
 
+
+let num = parseInt(Math.random() * 2) - 5; // 0 1 
+console.log(num);
+
+function getRandNum(min, max) {
+  return parseInt(Math.random() * (max - min + 1)) + min;
+}
+
+function checkNum(num) {
+  return num % 2 == 0 ? `${num} 은 짝수` : `${num} 은 홀수`;
+}
 /*
     1. 1~100 사이의 숫자를 랜덤으로 저장하고 그수가 짝수인지
       홀수인지 출력하는 checkNum 함수를 만든후 호출 
 */
-
+console.log(checkNum(getRandNum(1, 100)));
 /*  
      2. 배열에 랜덤으로(-100 ~ 100 사이의 숫자를 4개를 저장후 전부 홀수인지 검사하는 
        isAllOddNum 함수를 만드시오 , 전부 홀수입니다, 아닙니다 
  */
+
+function isAllOddNum() {
+  let arr = [0, 0, 0, 0];
+  for (idx in arr) {
+    arr[idx] = getRandNum(-100, 100);
+  }
+  arr = [1, -3, -99, 1];
+  console.log(arr);
+
+  let cnt = arr.reduce((cnt, value) => {
+    //console.log(`value = ${value} cnt = ${cnt}`);
+    return value % 2 !== 0 ? cnt += 1 : cnt;
+  }, 0);
+
+  return cnt === arr.length ? '전부 홀수값입니다' : '홀수가 아닌 값이 있습니다';
+
+}
+
+console.log(isAllOddNum());
