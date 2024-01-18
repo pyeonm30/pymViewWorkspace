f5a898764b3a1df1f3bd349803024a3802a2c057
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-01-18 16:08:31 +0900

    Update app.js

--- html-css-js/ex07Game1to50/app.js
+++ html-css-js/ex07Game1to50/app.js
@@ -7,14 +7,14 @@ class Game1To50 {
     this.recode = { min: 0, sec: 0, milSec: 0, cnt: 0 };
     this.gameTime = 0;
     this.gameBtn = document.querySelector("#start");
-    this.init();
-  }
-  init() {
     this.gameBtn.addEventListener('click', () => {
-
-    });
-
+      this.init();
+      this.gameStart();
+    })
   }
+
+  init() {};
+  gameStart() {};
 }
 
 const game = new Game1To50();
