263f889edb43749f8c7a5c889033e06afed2d196
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-01-19 13:56:59 +0900

    Create ex32ArrayFunc03.js

---
+++ javascriptEx/ex32ArrayFunc03.js
@@ -0,0 +1,22 @@
+let cat = {
+  kind: '고양이',
+  age: 5
+};
+let dog = {
+  kind: '개',
+  age: 4
+};
+let rabbit = {
+  kind: '토끼',
+  age: 0.5
+};
+let hamster = {
+  kind: '햄스터',
+  age: 1,
+  eat: function() {
+    console.log('해바라기씨를 먹는다 ')
+  }
+};
+
+
+let pets = [cat, dog, rabbit, hamster, cat];
