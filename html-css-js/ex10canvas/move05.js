b4f58514179b1d52c5f79608cc1e6c74cf042c37
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-01-22 18:10:27 +0900

    Update move05.js

--- html-css-js/ex10canvas/move05.js
+++ html-css-js/ex10canvas/move05.js
@@ -1,11 +1,11 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let key = { "ArrowRight": false, "ArrowLeft": false, "ArrowUp": false, "ArrowDown": false };
-let player = { "x": 375, "y": 375, "size": 50, "speed": 2, "color": "blue" };
+let player = { "x": 375, "y": 600 - 50, "size": 50, "speed": 2, "color": "blue" };
 let enemyList = [];
 let spotList = [];
 let timer = 0;
-let update = true;
+let isOver = false;
 
 function init() {
   document.addEventListener("keydown", e => keyHandler(e, true));
@@ -25,6 +25,18 @@ function setSpotList(size) {
   }
 }
 
+function movePlayer() {
+  if (key.ArrowRight && player.x < canvas.width - player.size) {
+    player.x += player.speed;
+  } else if (key.ArrowDown && player.y < canvas.height - player.size) {
+    player.y += player.speed;
+  } else if (key.ArrowUp && player.y > 0) {
+    player.y -= player.speed;
+  } else if (key.ArrowLeft && player.x > 0) {
+    player.x -= player.speed;
+  }
+}
+
 function drawObj(obj) {
   ctx.beginPath();
   ctx.rect(obj.x, obj.y, obj.size, obj.size);
@@ -37,27 +49,81 @@ function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   spotList.forEach(spot => drawObj(spot));
   drawObj(player);
+  createEnemyList();
+  enemyList.forEach(emeny => drawObj(emeny));
+
+  if (isOver) clearInterval(interval);
+  movePlayer();
+  moveEnemyList();
+  UpdateEnemyList();
+  isCollison();
+
+
 }
 // 1초에 랜덤 spot에서 emeny를 3개씩 생성하라 
 function createEnemyList() {
-  for (let i = 0; i < 3; i += 1) {
-    let spot = spotList[0];
-    let enemy = { x: spot.x, y: spot.y, size: 80, speed: 2, color: 'green', 'alive': 1000 }
+  if (timer % 100 == 0) {
+    timer = 0;
+    for (let i = 0; i < 3; i += 1) {
+      let rIdx = parseInt(Math.random() * spotList.length);
+      let spot = spotList[rIdx];
+      let rSpeed = parseInt(Math.random() * 4) + 1; // 1~4px 
+      let enemy = { x: spot.x, y: spot.y, size: 80, speed: rSpeed, color: 'green', 'alive': 1000 }
+      enemyList.push(enemy);
+    }
   }
+  timer += 1;
 
 }
 
 function moveEnemyList() {
   enemyList.forEach(
     enemy => {
+      enemy.y += enemy.speed;
       enemy.alive -= 1;
     }
   )
 }
 
-function deleteDeadEnemy() {
+function UpdateEnemyList() {
   //alive  <=  0 enemy 배열에서 삭제 
+  enemyList = enemyList.filter(emeny => emeny.alive > 0);
 }
+
+// function inEnemy(px, py, enemy) {
+//   return (enemy.x < px && px < enemy.x + enemy.size) &&
+//     (enemy.y < py && py < enemy.y + enemy.size)
+// }
+
+// function collison(enemy) {
+//   if (inEnemy(player.x, player.y, enemy)) return true;
+//   else if (inEnemy(player.x + player.size, player.y, enemy)) return true;
+//   else if (inEnemy(player.x, player.y + player.size, enemy)) return true;
+//   else if (inEnemy(player.x + player.size, player.y + player.size, enemy)) return true;
+//   else return false;
+// }
+function collison(enemy) {
+  if (player.x < enemy.x + enemy.size &&
+    player.y < enemy.y + enemy.size &&
+    player.x + player.size > enemy.x &&
+    player.y + player.size > enemy.y
+  ) { return true; }
+  return false;
+}
+
+function isCollison() {
+  //collison() ? enemy.color = 'red' : enemy.color = 'green';
+  enemyList.forEach(
+    enemy => {
+      if (collison(enemy)) {
+        enemy.color = 'red'
+        isOver = true;
+        //clearInterval(interval);
+      } else { enemy.color = 'green' }
+    });
+}
+
+
 //---------
 init();
-draw();
+let interval = setInterval(draw, 10);
