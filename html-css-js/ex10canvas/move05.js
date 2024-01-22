c0c586086a9cdc80f4a6f2330688594f9d03d05e
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-01-22 17:05:42 +0900

    Update move05.js

--- html-css-js/ex10canvas/move05.js
+++ html-css-js/ex10canvas/move05.js
@@ -1,4 +1,4 @@
-let canvas = document.getElementById("myCanvas");
+let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let key = { "ArrowRight": false, "ArrowLeft": false, "ArrowUp": false, "ArrowDown": false };
 let player = { "x": 375, "y": 375, "size": 50, "speed": 2, "color": "blue" };
@@ -18,3 +18,26 @@ function keyHandler(e, value) {
     key[e.key] = value;
   }
 }
+
+function setSpotList(size) {
+  for (let i = 0; i < size; i += 1) {
+    spotList.push({ x: 45 + i * 100, y: 50, size: 10, color: 'blue' });
+  }
+}
+
+function drawObj(obj) {
+  ctx.beginPath();
+  ctx.rect(obj.x, obj.y, obj.size, obj.size);
+  ctx.fillStyle = obj.color;
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  spotList.forEach(spot => drawObj(spot));
+  drawObj(player);
+}
+//---------
+init();
+draw();
