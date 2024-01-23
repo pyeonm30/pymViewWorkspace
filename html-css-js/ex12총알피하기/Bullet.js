f448b7db11f630d97682c542128d7a579003c87e
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-01-23 11:41:16 +0900

    .
    .

---
+++ html-css-js/ex12총알피하기/Bullet.js
@@ -0,0 +1,36 @@
+class Bullet {
+  constructor() {
+    this.radius = 7;
+    this.speed = 2;
+    this.x; // 총알의 시작점
+    this.y;
+    this.dx; // 총알의 끝점 
+    this.dy;
+    //캔버스크기 
+    this.color = "green";
+    this.cwidth = 800;
+    this.cheight = 400;
+  }
+
+  init() {
+    // 동서남북 4개의 방향에서 랜덤으로 총알이 나온다 
+    let location = parseInt(Math.random() * 4); // 0.1234 , 1.1234,2.1234,3.1234 // 0 1 2 3 
+
+    // 총알이 캔버스 위에서 시작
+    if (location == 0) {
+      this.x = Math.random() * (this.cwidth - this.radius); // 0~ 799
+      this.y = this.radius;
+    } else if (location == 1) {
+      // 총알이 캔버스 아래에서 시작
+
+
+    } else if (location == 2) {
+      // 총알이 왼쪽에서 시작 
+
+    } else if (location == 3) {
+      // 총알이 오른쪽에서 시작 
+
+    }
+  }
+
+}
---
+++ html-css-js/ex12총알피하기/game.js
@@ -0,0 +1,60 @@
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext('2d');
+// 정중앙에 위치시키기
+let player = { x: canvas.width / 2 - 25, y: canvas.height / 2 - 25, size: 50, speed: 3 };
+let key = {
+  ArrowRight: false,
+  ArrowLeft: false,
+  ArrowUp: false,
+  ArrowDown: false
+}
+let imgReady = false;
+
+let playerImg = new Image();
+playerImg.src = './bug.png';
+
+function init() {
+  playerImg.addEventListener("load", () => {
+    imgReady = true;
+  })
+  document.addEventListener("keydown", e => keyHandler(e, true));
+  document.addEventListener("keyup", e => keyHandler(e, false));
+}
+
+function drawPlayer() {
+  ctx.beginPath();
+  //이미지객체 , 시작좌표x, y  이미지크기 가로   세로 
+  //ctx.rect(player.x, player.y, player.size, player.size);
+  ctx.drawImage(playerImg, player.x, player.y, player.size, player.size);
+  ctx.fill();
+  ctx.closePath();
+}
+
+
+function keyHandler(e, value) {
+  if (key[e.key] !== undefined) {
+    key[e.key] = value;
+  }
+}
+
+function movePlayer() {
+  if (key.ArrowRight && player.x < canvas.width - player.size) {
+    player.x += player.speed;
+  } else if (key.ArrowDown && player.y < canvas.height - player.size) {
+    player.y += player.speed;
+  } else if (key.ArrowUp && player.y > 0) {
+    player.y -= player.speed;
+  } else if (key.ArrowLeft && player.x > 0) {
+    player.x -= player.speed;
+  }
+}
+
+
+function render() {
+  if (!imgReady) return;
+  drawPlayer();
+  // imgReady && drawPlayer();
+}
+
+init();
+setInterval(render, 10)
